fc3b47399f9173e489eeb4cde238939fe7bec88d
Palanivel1116 <palanivel@example.com>
2025-11-17 14:57:36 +0530

    Updated the code

--- src/component/Project.js
+++ src/component/Project.js
@@ -23,7 +23,7 @@ const Project = () => {
 
             <div className="absolute inset-0 flex flex-col items-center justify-center text-center 
                             bg-gray-400 opacity-0 group-hover:opacity-100 transition-all duration-500 ease-in-out">
-              <h5 className="text-2xl font-bold mb-3 bg-transparent text-white">Project 1</h5>
+              <h5 className="text-2xl font-bold mb-3 bg-transparent text-white">Online Herbs Shopping</h5>
               <p className="text-white text-lg px-6 bg-transparent">
 I created an Online Herbs Shopping website using HTML, CSS, and JavaScript to provide a simple and elegant way to explore and purchase herbal products. The website includes a clean layout, product cards, and responsive design for different devices. Through this project, I improved my front-end development skills and learned how to design attractive user interfaces with interactive features and smooth animations.
               </p>
@@ -43,7 +43,7 @@ I created an Online Herbs Shopping website using HTML, CSS, and JavaScript to pr
 
             <div className="absolute inset-0 flex flex-col items-center justify-center text-center 
                             bg-gray-400 opacity-0 group-hover:opacity-100 transition-all duration-500 ease-in-out">
-              <h5 className="text-2xl font-bold mb-3 bg-transparent text-white">Project 2</h5>
+              <h5 className="text-2xl font-bold mb-3 bg-transparent text-white">Online Food Delivery</h5>
               <p className="text-white bg-transparent text-lg px-6">
                 I have completed a project called "Food Delivery Website" where I used React, JavaScript, HTML, and CSS to develop a responsive and interactive platform. The website allows users to explore various food items, add them to the cart, and view dynamic updates in real-time. Through this project, I gained hands-on experience with React components, state management, and responsive web design, enhancing both functionality and user experience across different devices.
               </p>
